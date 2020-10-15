72aa222c84403d3779718b7d8da10167cd5f9417
romainmenke <romainmenke@gmail.com>
2020-10-15 12:03:07 +0200

    fix indent

--- without-feature-detect.js
+++ without-feature-detect.js
@@ -1,67 +1,67 @@
 // Exact copy of "index.js" without the feature detection.
 (function (ElementPrototype) {
-		// scope regex
-		var scope = /:scope(?![\w-])/gi;
+	// scope regex
+	var scope = /:scope(?![\w-])/gi;
 
-		// polyfill Element#querySelector
-		var querySelectorWithScope = polyfill(ElementPrototype.querySelector);
+	// polyfill Element#querySelector
+	var querySelectorWithScope = polyfill(ElementPrototype.querySelector);
 
-		ElementPrototype.querySelector = function querySelector(selectors) {
-			return querySelectorWithScope.apply(this, arguments);
+	ElementPrototype.querySelector = function querySelector(selectors) {
+		return querySelectorWithScope.apply(this, arguments);
+	};
+
+	// polyfill Element#querySelectorAll
+	var querySelectorAllWithScope = polyfill(ElementPrototype.querySelectorAll);
+
+	ElementPrototype.querySelectorAll = function querySelectorAll(selectors) {
+		return querySelectorAllWithScope.apply(this, arguments);
+	};
+
+	// polyfill Element#matches
+	if (ElementPrototype.matches) {
+		var matchesWithScope = polyfill(ElementPrototype.matches);
+
+		ElementPrototype.matches = function matches(selectors) {
+			return matchesWithScope.apply(this, arguments);
 		};
+	}
 
-		// polyfill Element#querySelectorAll
-		var querySelectorAllWithScope = polyfill(ElementPrototype.querySelectorAll);
+	// polyfill Element#closest
+	if (ElementPrototype.closest) {
+		var closestWithScope = polyfill(ElementPrototype.closest);
 
-		ElementPrototype.querySelectorAll = function querySelectorAll(selectors) {
-			return querySelectorAllWithScope.apply(this, arguments);
+		ElementPrototype.closest = function closest(selectors) {
+			return closestWithScope.apply(this, arguments);
 		};
+	}
 
-		// polyfill Element#matches
-		if (ElementPrototype.matches) {
-			var matchesWithScope = polyfill(ElementPrototype.matches);
+	function polyfill(qsa) {
+		return function (selectors) {
+			// whether the selectors contain :scope
+			var hasScope = selectors && scope.test(selectors);
 
-			ElementPrototype.matches = function matches(selectors) {
-				return matchesWithScope.apply(this, arguments);
-			};
-		}
+			if (hasScope) {
+				// fallback attribute
+				var attr = 'q' + Math.floor(Math.random() * 9000000) + 1000000;
 
-		// polyfill Element#closest
-		if (ElementPrototype.closest) {
-			var closestWithScope = polyfill(ElementPrototype.closest);
+				// replace :scope with the fallback attribute
+				arguments[0] = selectors.replace(scope, '[' + attr + ']');
 
-			ElementPrototype.closest = function closest(selectors) {
-				return closestWithScope.apply(this, arguments);
-			};
-		}
+				// add the fallback attribute
+				this.setAttribute(attr, '');
 
-		function polyfill(qsa) {
-			return function (selectors) {
-				// whether the selectors contain :scope
-				var hasScope = selectors && scope.test(selectors);
+				// results of the qsa
+				var elementOrNodeList = qsa.apply(this, arguments);
 
-				if (hasScope) {
-					// fallback attribute
-					var attr = 'q' + Math.floor(Math.random() * 9000000) + 1000000;
+				// remove the fallback attribute
+				this.removeAttribute(attr);
 
-					// replace :scope with the fallback attribute
-					arguments[0] = selectors.replace(scope, '[' + attr + ']');
-
-					// add the fallback attribute
-					this.setAttribute(attr, '');
-
-					// results of the qsa
-					var elementOrNodeList = qsa.apply(this, arguments);
-
-					// remove the fallback attribute
-					this.removeAttribute(attr);
-
-					// return the results of the qsa
-					return elementOrNodeList;
-				} else {
-					// return the results of the qsa
-					return qsa.apply(this, arguments);
-				}
-			};
-		}
-	})(Element.prototype);
+				// return the results of the qsa
+				return elementOrNodeList;
+			} else {
+				// return the results of the qsa
+				return qsa.apply(this, arguments);
+			}
+		};
+	}
+})(Element.prototype);
